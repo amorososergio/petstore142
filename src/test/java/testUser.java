1700e36722aad41f5f639ac588713fa5f804a2ab
amorososergio <sergioamorosoj@gmail.com>
2025-03-12 19:54:44 -0300

    first commit

---
+++ src/test/java/testUser.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class testUser {
+    static  String ct = "application/json";
+    static  String url = "http://petstore.swagger.io/v2/user";
+
+
+public static String lerArquivoJson(String arquivoJson) {
+    return null;
+    //return new String(Files.readAllBytes(Paths.get(arquivoJson)));
+}
+
+    @Test
+    public void testPostUser() {
+        // String jsonBody
+    }
+}
