877024d4e76ba1a994debd0cb191e78e6c6c7c2c
amorososergio <sergioamorosoj@gmail.com>
2025-03-17 17:36:08 -0300

    novasFuncoes

--- src/test/java/testUser.java
+++ src/test/java/testUser.java
@@ -1,17 +1,40 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
 
 public class testUser {
     static  String ct = "application/json";
-    static  String url = "http://petstore.swagger.io/v2/user";
+    static  String urlPet = "https://petstore.swagger.io/v2/pet";
 
 
-public static String lerArquivoJson(String arquivoJson) {
-    return null;
-    //return new String(Files.readAllBytes(Paths.get(arquivoJson)));
+public static String lerArquivoJson(String arquivoJson) throws IOException {
+    return new String(Files.readAllBytes(Paths.get(arquivoJson)));
 }
 
     @Test
-    public void testPostUser() {
-        // String jsonBody
+    public void testPostUser() throws IOException {
+        String jsonBody = lerArquivoJson("src/test/resources/json/pet1.json");
+        int petId = 15899999;
+
+        given()
+            .contentType(ct)
+            .log().all()
+            .body(jsonBody)
+        .when()
+            .post(urlPet)
+        .then()
+            .log().all()
+            .statusCode(200)
+            .body("name", is("Kiara"))
+            .body("id", is(petId))
+            .body("status", is("available"))
+            .body("category.name", is("cachorro"))
+            .body("tags[0].name", is("vacinado"))
+        ;
     }
 }
